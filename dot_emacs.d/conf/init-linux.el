b711c5100ce4f65a93358ccac3caadf09e5492d0
Kimihiko Shimomura <kshimo69@gmail.com>
2010-08-02 19:35:26 +0900

    font setting for linux.

--- dot_emacs.d/conf/init-linux.el
+++ dot_emacs.d/conf/init-linux.el
@@ -9,49 +9,26 @@
 (if window-system
     (progn
       (menu-bar-mode t)
-      ;; ;; GUIの場合はM+2VM+IPAG cicleを指定
-      ;; (set-default-font "M+2VM+IPAG circle-16")
-      ;; (set-frame-font "M+2VM+IPAG circle-16")
-      ;; ;; (set-default-font "VL ゴシック-14")
-      ;; ;; (set-frame-font "VL ゴシック-14")
-      ;; ;; (set-frame-font "Bitstream Vera Sans Mono-14")
-      ;; (set-fontset-font (frame-parameter nil 'font)
-      ;;                   'japanese-jisx0208
-      ;;                   '("M+2VM+IPAG circle-16" . "unicode-bmp"))
-      ;;                   ;; '("VL ゴシック-14" . "unicode-bmp"))
-      ;; (set-fontset-font (frame-parameter nil 'font)
-      ;;                   'katakana-jisx0201
-      ;;                   '("M+2VM+IPAG circle-16" . "unicode-bmp"))
-      ;;                   ;; '("VL ゴシック-14" . "unicode-bmp"))
-      ;; (set-fontset-font (frame-parameter nil 'font)
-      ;;                   'ascii
-      ;;                   '("M+2VM+IPAG circle-16" . "unicode-bmp"))
-      ;;                   ;; '("VL ゴシック-14" . "unicode-bmp"))
-      ;; (set-fontset-font (frame-parameter nil 'font)
-      ;;                   'unicode
-      ;;                   '("M+2VM+IPAG circle-16" . "unicode-bmp"))
-      ;;                   ;; '("VL ゴシック-14" . "unicode-bmp"))
 
-      ;; http://emacs.g.hatena.ne.jp/sakito/20100127
-      (create-fontset-from-ascii-font "VL ゴシック-14:weight=normal:slant=normal:spacing=m" nil "vlipagothic")
-      (set-fontset-font "fontset-vlipagothic"
-                        'unicode
-                        (font-spec :family "M+2VM+IPAG circle" :spacing 'm)
-                        nil
-                        'append)
-      ;; ひらがな、カタカナ は M+2VM+IPAG circle
-      (set-fontset-font "fontset-vlipagothic"
-                        '( #x3000 .  #x30ff)
-                        (font-spec :family "M+2VM+IPAG circle" :spacing 'm)
-                        nil
-                        'prepend)
-      ;; 半角カタカナ、全角アルファベット は M+2VM+IPAG circle
-      (set-fontset-font "fontset-vlipagothic"
-                        '( #xff00 .  #xffef)
-                        (font-spec :family "M+2VM+IPAG circle" :spacing 'm)
-                        nil
-                        'prepend)
-      (add-to-list 'default-frame-alist '(font . "fontset-vlipagothic"))
+      ;; (set-frame-font "Bitstream Vera Sans Mono-14")
+      (set-face-attribute 'default nil
+                          :family "Bitstream Vera Sans Mono"
+                          :height 140)
+      (set-fontset-font
+       (frame-parameter nil 'font)
+       'japanese-jisx0208
+       '("MigMix 2M" . "iso10646-1"))
+      (set-fontset-font
+       (frame-parameter nil 'font)
+       'japanese-jisx0212
+       '("MigMix 2M" . "iso10646-1"))
+      (set-fontset-font
+       (frame-parameter nil 'font)
+       'mule-unicode-0100-24ff
+       '("Bitstream Vera Sans Mono" . "iso10646-1"))
+      (setq face-font-rescale-alist
+            '((".*MigMix 2M.*" . 1.25)
+              ("-cdac$" . 1.3)))
 
       ;; 最初のフレーム
       (setq initial-frame-alist
@@ -61,8 +38,8 @@
                      '(cursor-color . "orange")
                      '(mouse-color . "orange")
                      '(vertical-scroll-bars . nil)
-                     '(width . 200)                   ;; フレームの幅
-                     '(height . 50)                  ;; フレームの高さ
+                     '(width . 166)                   ;; フレームの幅
+                     '(height . 55)                  ;; フレームの高さ
                      '(top . 50)                     ;; Y 表示位置
                      '(left . 200)                   ;; X 表示位置
                      '(alpha . (85 20))
@@ -81,12 +58,10 @@
                      '(vertical-scroll-bars . nil)
                      '(width . 80)                   ;; フレームの幅
                      '(height . 35)                  ;; フレームの高さ
-                     ;; '(font . "M+2VM+IPAG circle-16")
-                     ;; '(font . "VL ゴシック-14")
                      )
                     default-frame-alist)
             )
-      (setq-default line-spacing 1)
+      ;; (setq-default line-spacing 1)
       ))
 
 (provide 'init-linux)
