6e9c96fc2be7e95168584d5f775f38e0a67143f4
Kimihiko Shimomura <kshimo69@gmail.com>
2010-08-17 16:22:48 +0900

    change twittering-mode setting.

--- dot_emacs.d/conf/init-twitter.el
+++ dot_emacs.d/conf/init-twitter.el
@@ -1,8 +1,17 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 ;; http://www.emacswiki.org/emacs/TwitteringMode
+;; http://sourceforge.net/projects/twmode/files/
 
 (require 'twittering-mode)
+
+;;; server setting
+;; (setq twittering-api-host "kshimo69-gtap.appspot.com/api/")
+;; (setq twittering-api-search-host "kshimo69-gtap.appspot.com/")
+;; (setq twittering-web-host "kshimo69-gtap.appspot.com/")
+
+;;; user setting
 (setq twittering-username "kshimo69")
+(setq twittering-retweet-format "RT: @%s: %t")
 
 (add-hook 'twittering-mode-hook
           (lambda ()
@@ -14,14 +23,14 @@
 (twittering-icon-mode)
 (setq twittering-timer-interval 120)
 
-(add-hook 'twittering-new-tweets-hook
-          (lambda ()
-            (let ((n twittering-new-tweets-count))
-              (start-process "twittering-notify" nil "growlnotify"
-                             "-n" "New Tweet"
-                             "--image" "/Applications/Emacs.app/Contents/Resources/Emacs.icns"
-                             "-m"
-                             (format "You have %d new tweet%s"
-                                     n (if (> n 1) "s" ""))))))
+;; (add-hook 'twittering-new-tweets-hook
+;;           (lambda ()
+;;             (let ((n twittering-new-tweets-count))
+;;               (start-process "twittering-notify" nil "growlnotify"
+;;                              "-n" "New Tweet"
+;;                              "--image" "/Applications/Emacs.app/Contents/Resources/Emacs.icns"
+;;                              "-m"
+;;                              (format "You have %d new tweet%s"
+;;                                      n (if (> n 1) "s" ""))))))
 
 (provide 'init-twitter)
