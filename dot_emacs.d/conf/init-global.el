8cff7d92a444017973171babf6be447aa0e6cd38
Kimihiko Shimomura <kshimo69@gmail.com>
2010-08-22 00:27:25 +0900

    add bookmark configuration

--- dot_emacs.d/conf/init-global.el
+++ dot_emacs.d/conf/init-global.el
@@ -191,6 +191,22 @@
 (setq recentf-exclude '("/GTAGS$"))
 (require 'recentf-ext)
 
+;; ブックマークを変更したら即保存する
+(setq bookmark-save 1)
+;; ブックマークの使い方
+;; ブックマークの設定
+;; C-x r m (bookmark-set)
+;; ブックマーク選択メニューの表示
+;; C-x r l (bookmark-bmenu-list)
+;; 超整理法
+(progn
+  (setq bookmark-sort-flag nil)
+  (defun bookmark-arrange-latest-top ()
+    (let ((latest (bookmark-get-bookmark bookmark)))
+      (setq bookmark-alist (cons latest (delq latest bookmark-alist))))
+    (bookmark-save))
+  (add-hook 'bookmark-after-jump-hook 'bookmark-arrange-latest-top))
+
 ;; buffer切り替えを使い易く
 (iswitchb-mode 1)
 ;; C-f, C-b, C-n, C-p で候補を切り替えることができるように。
