8ebb0a43151def1d1c513eb6c84f3a815e7aacff
Kimihiko Shimomura <kshimo69@gmail.com>
2010-08-27 01:01:58 +0900

    add more grep command

--- dot_emacs.d/conf/init-global.el
+++ dot_emacs.d/conf/init-global.el
@@ -319,6 +319,26 @@
                        command (car kill-ring)))))
 (global-set-key (kbd "C-c g") 'grep-find-current-word)
 
+;; igrep - 対話的にgrepする
+;; (auto-install-from-emacswiki "igrep.el")
+(when (require 'igrep)
+  ;; lgrepに-0u8オプションをつけると出力がutf-8になる
+  (igrep-define lgrep (igrep-use-zgrep nil)(ignore-regex-option "-n -0u8"))
+  (igrep-find-define lgrep (igrep-use-zgrep nil)(ignore-regex-option "-n -0u8"))
+  )
+
+;; 複数のgrepバッファを使う
+;; (auto-install-from-emacswiki "grep-a-lot.el")
+(when (require 'grep-a-lot)
+  (grep-a-lot-setup-keys)
+  ;; igrep用
+  (grep-a-lot-advise igrep)
+  )
+
+;; grepの検索結果を編集する
+;; (auto-install-from-emacswiki "grep-edit.el")
+(when (require 'grep-edit))
+
 ;; color-moccur
 ;; (auto-install-from-emacswiki "color-moccur.el")
 ;; (auto-install-from-emacswiki "moccur-edit.el")
