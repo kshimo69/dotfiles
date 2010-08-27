46f341d67fe836436809178ddbf9cc44ff234bf1
Kimihiko Shimomura <kshimo69@gmail.com>
2010-08-27 19:21:06 +0900

    undefined lgrep

--- dot_emacs.d/conf/init-global.el
+++ dot_emacs.d/conf/init-global.el
@@ -322,17 +322,17 @@
 ;; igrep - 対話的にgrepする
 ;; (auto-install-from-emacswiki "igrep.el")
 (when (require 'igrep)
-  ;; lgrepに-0u8オプションをつけると出力がutf-8になる
-  (igrep-define lgrep (igrep-use-zgrep nil)(ignore-regex-option "-n -0u8"))
-  (igrep-find-define lgrep (igrep-use-zgrep nil)(ignore-regex-option "-n -0u8"))
+;; ;; lgrepに-0u8オプションをつけると出力がutf-8になる
+;; (igrep-define lgrep (igrep-use-zgrep nil)(ignore-regex-option "-n -0u8"))
+;; (igrep-find-define lgrep (igrep-use-zgrep nil)(ignore-regex-option "-n -0u8"))
   )
 
 ;; 複数のgrepバッファを使う
 ;; (auto-install-from-emacswiki "grep-a-lot.el")
 (when (require 'grep-a-lot)
   (grep-a-lot-setup-keys)
-  ;; igrep用
-  (grep-a-lot-advise igrep)
+  ;; ;; igrep用
+  ;; (grep-a-lot-advise igrep)
   )
 
 ;; grepの検索結果を編集する
