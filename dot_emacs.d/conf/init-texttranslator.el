1a7ded11f0fb242f819934869ba4f057d4a8b78a
Kimihiko Shimomura <kshimo69@gmail.com>
2010-08-29 22:08:40 +0900

    update text-translator

--- dot_emacs.d/conf/init-texttranslator.el
+++ dot_emacs.d/conf/init-texttranslator.el
@@ -1,12 +1,10 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
-;; (auto-install-from-emacswiki "text-translator.el")
-;; (auto-install-from-emacswiki "text-translator-vars.el")
-;; (auto-install-from-emacswiki "text-translator-load.el")
+;; (auto-install-batch "text translator")
 
 ;; http://d.hatena.ne.jp/khiker/20070503/emacs_text_translator
 (require 'text-translator)
-(global-set-key (kbd "C-x M-t") 'text-translator)
-(global-set-key (kbd "C-x M-T") 'text-translator-translate-last-string)
+;; (global-set-key (kbd "C-x M-t") 'text-translator)
+;; (global-set-key (kbd "C-x M-T") 'text-translator-translate-last-string)
 (setq text-translator-default-engine "google.com_enja")
 
 ;; 自動選択に使用する関数を設定
