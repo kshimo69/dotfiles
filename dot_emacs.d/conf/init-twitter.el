cc75b1047cfb01abe8a4d01538f1d74a4bf240f9
Kimihiko Shimomura <kshimo69@gmail.com>
2010-08-30 18:17:17 +0900

    add notification and some hooks on twittering-mode

--- dot_emacs.d/conf/init-twitter.el
+++ dot_emacs.d/conf/init-twitter.el
@@ -6,19 +6,40 @@
 
 (setq twittering-username "kshimo69")
 (setq twittering-status-format
-      "%C{%Y/%m/%d %H:%M:%S} %s > %T // from %f%L%r%R")
+      ;; "%C{%Y/%m/%d %H:%M:%S} %s > %T // from %f%L%r%R")
+      "%C{%H:%M:%S} %s > %T [from %f]%r%R")
 (setq twittering-retweet-format "RT: @%s: %t")
 ;; (setq twittering-reverse-mode t)
-;; (setq twittering-timer-interval 60)
+(setq twittering-timer-interval 60)
 
-;; (add-hook 'twittering-new-tweets-hook
-;;           (lambda ()
-;;             (let ((n twittering-new-tweets-count))
-;;               (start-process "twittering-notify" nil "growlnotify"
-;;                              "-n" "New Tweet"
-;;                              "--image" "/Applications/Emacs.app/Contents/Resources/Emacs.icns"
-;;                              "-m"
-;;                              (format "You have %d new tweet%s"
-;;                                      n (if (> n 1) "s" ""))))))
+(defun twittering-my-list-timeline ()
+  (interactive)
+  (twittering-visit-timeline '"kshimo69/jazzout"))
+
+(add-hook 'twittering-mode-hook
+          (lambda ()
+            (mapc (lambda (pair)
+                    (let ((key (car pair))
+                          (func (cdr pair)))
+                      (define-key twittering-mode-map
+                        (read-kbd-macro key) func)))
+                  '(("F" . twittering-friends-timeline)
+                    ("R" . twittering-replies-timeline)
+                    ("U" . twittering-user-timeline)
+                    ("J" . twittering-my-list-timeline)
+                    ("W" . twittering-update-status-interactive)))))
+
+(defvar twittering-notify-keyword "^.*\\(kshimo69\\|jazzout\\).*$")
+(add-hook 'twittering-new-tweets-hook
+          (lambda ()
+            (dolist (el twittering-new-tweets-statuses)
+              (if (string-match twittering-notify-keyword (cdr (assoc 'text el)))
+            (start-process "twittering-notify" nil "notify-send"
+                           "-i" "/usr/share/pixmaps/redhat/shadowman-transparent-64.png"
+                           "Match keyword"
+                           (format "%s: %s"
+                                   (cdr (assoc 'user-screen-name el))
+                                   (cdr (assoc 'text el))
+                                   ))))))
 
 (provide 'init-twitter)
