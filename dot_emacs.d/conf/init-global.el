6a3c47fba290ba69dd9bf8449015c1152bc7e4b1
Kimihiko Shimomura <kshimo69@gmail.com>
2010-09-22 15:33:39 +0900

    disable eldoc

--- dot_emacs.d/conf/init-global.el
+++ dot_emacs.d/conf/init-global.el
@@ -466,12 +466,12 @@
 
 ;; Emacs Lisp関数・変数のヘルプをエコーエリアに表示する
 ;; (auto-install-from-emacswiki "eldoc-extension.el")
-(require 'eldoc-extension)
-(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
-(setq eldoc-idle-delay 0.4)
-(setq eldoc-minor-mode-string "")
+;; (require 'eldoc-extension)
+;; (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+;; (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+;; (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+;; (setq eldoc-idle-delay 0.4)
+;; (setq eldoc-minor-mode-string "")
 
 ;; Emacs Lisp式の値をコメントで注釈する
 ;; (auto-install-from-emacswiki "lispxmp.el")
