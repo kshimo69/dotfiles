6273cf7a183bdd4abc282d6786e822d89c25af68
Kimihiko Shimomura <kshimo69@gmail.com>
2011-12-10 17:56:55 +0900

    add url.

--- .emacs.d/conf/init-rails.el
+++ .emacs.d/conf/init-rails.el
@@ -1,5 +1,7 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 ;; http://d.hatena.ne.jp/willnet/20090110/1231595231
+;; http://d.hatena.ne.jp/authorNari/20090611/1244732270
+
 ;; Interactively Do Things (highly recommended, but not strictly required)
 ;; (require 'ido)
 ;; (ido-mode t)
