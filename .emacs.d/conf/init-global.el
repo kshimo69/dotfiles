d712e6811e2feb30f74662ca0a9ea73ace047731
Kimihiko Shimomura <kshimo69@gmail.com>
2011-12-19 22:21:46 +0900

    add dired setting.

--- .emacs.d/conf/init-global.el
+++ .emacs.d/conf/init-global.el
@@ -425,4 +425,28 @@
 (setq-default header-line-format '(which-func-mode ("" which-func-format)))
 (set-face-foreground 'which-func "pink")
 
+;; http://masutaka.net/chalow/2011-12-17-1.html
+(defface dired-todays-face '((t (:foreground "green"))) nil)
+(defvar dired-todays-face 'dired-todays-face)
+(defconst month-name-alist
+  '(("1"  . "Jan") ("2"  . "Feb") ("3"  . "Mar") ("4"  . "Apr")
+    ("5"  . "May") ("6"  . "Jun") ("7"  . "Jul") ("8"  . "Aug")
+    ("9"  . "Sep") ("10" . "Oct") ("11" . "Nov") ("12" . "Dec")))
+(defun dired-today-search (arg)
+  "Fontlock search function for dired."
+  (search-forward-regexp
+   (let ((month-name
+          (cdr (assoc (format-time-string "%b") month-name-alist))))
+     (if month-name
+         (format
+          (format-time-string
+           "\\(%Y-%m-%d\\|%b %e\\|%%s %e\\) [0-9]....") month-name)
+       (format-time-string
+        "\\(%Y-%m-%d\\|%b %e\\) [0-9]....")))
+   arg t))
+(eval-after-load "dired"
+  '(font-lock-add-keywords
+    'dired-mode
+    (list '(dired-today-search . dired-todays-face))))
+
 (provide 'init-global)
