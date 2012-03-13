7f291d3dbfb341da2ee0e90cbad0d95c56719cd9
Kimihiko Shimomura <kshimo69@gmail.com>
2012-03-13 23:06:37 +0900

    update tempbuf.el

--- .emacs.d/conf/init-global.el
+++ .emacs.d/conf/init-global.el
@@ -109,14 +109,14 @@
         ("." . "~/.emacs.d/var/emacs")
         ))
 
-;; ;; 使わないバッファを自動的に消す
-;; ;; (auto-install-from-emacswiki "tempbuf.el")
-;; (require 'tempbuf)
-;; (setq tempbuf-life-extension-ratio 100)
-;; ;; ファイルを開いたら自動的にtempbufを有効にする
-;; (add-hook 'find-file-hooks 'turn-on-tempbuf-mode)
-;; ;; diredバッファに対してtempbufを有効にする
-;; (add-hook 'dired-mode-hook 'turn-on-tempbuf-mode)
+;; 使わないバッファを自動的に消す
+;; (auto-install-from-emacswiki "tempbuf.el")
+(require 'tempbuf)
+(setq tempbuf-life-extension-ratio 100)
+;; ファイルを開いたら自動的にtempbufを有効にする
+(add-hook 'find-file-hooks 'turn-on-tempbuf-mode)
+;; diredバッファに対してtempbufを有効にする
+(add-hook 'dired-mode-hook 'turn-on-tempbuf-mode)
 
 ;; ファイル保存時に自動的にバイトコンパイルする
 ;; (auto-install-from-emacswiki "auto-async-byte-compile.el")
