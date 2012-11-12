c56cb92a620bddc8b381416dad19e03caa18e913
Kimihiko Shimomura <kshimo69@gmail.com>
2012-11-12 14:34:31 +0900

    PATHの設定を後でもやってみる

--- .emacs.d/conf/init-nt.el
+++ .emacs.d/conf/init-nt.el
@@ -3,5 +3,25 @@
 (require 'cygwin-mount)
 (cygwin-mount-activate)
 
+;; exec-path、PATH、MANPATHの追加 (下が優先)
+;; http://sakito.jp/emacs/emacsshell.html
+(dolist (dir (list
+              "/sbin"
+              "/bin"
+              "/usr/sbin"
+              "/usr/bin"
+              "/usr/local/sbin"
+              "/usr/local/bin"
+              "/opt/local/sbin"
+              "/opt/local/bin"
+              (expand-file-name "~/local/bin")
+              (expand-file-name "~/bin")
+              ))
+  (when (and (file-exists-p dir) (not (member dir exec-path)))
+    (setenv "PATH" (concat dir ":" (getenv "PATH")))
+    (setq exec-path (append (list dir) exec-path))))
+(setenv "MANPATH"
+        (concat "/usr/local/man:/usr/share/man:/Developer/usr/share/man:/sw/share/man"
+                                                                                                                                                                                            (getenv "MANPATH")))
 (provide 'init-nt)
 
