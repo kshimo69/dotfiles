a9ff6d9ad9c2f1637bef819b06d950dc9e47c3ee
Kimihiko Shimomura <kshimo69@gmail.com>
2015-02-27 13:53:29 +0900

    logger設定を追加

--- .vim/template/template.py
+++ .vim/template/template.py
@@ -5,4 +5,14 @@
 # License:  MIT License
 # Created:  <+DATE+>
 #
+
+from logging import getLogger,StreamHandler,DEBUG
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+
+logger.debug('hello')
+
 <+CURSOR+>
