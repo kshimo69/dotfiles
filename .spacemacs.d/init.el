3954d57b8a551ccbe9eaf930885eca1a216c318a
Kimihiko Shimomura <kshimo69@gmail.com>
2017-04-12 15:10:24 +0900

    auto-completionは言語のメジャーモードより後にしないとダメらしい

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -38,10 +38,6 @@ values."
      ;; ----------------------------------------------------------------
      osx
      helm
-     (auto-completion :variables
-                      auto-completion-enable-snippets-in-popup t
-                      auto-completion-enable-help-tooltip t
-                      )
      ;; better-defaults
      emacs-lisp
      git
@@ -67,6 +63,10 @@ values."
      html
      python
      ;; plantuml
+     (auto-completion :variables
+                      auto-completion-enable-snippets-in-popup t
+                      auto-completion-enable-help-tooltip t
+                      )
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
