d7e590f79f51f5e98aa6b2fad46b99121b304e4c
Kimihiko Shimomura <kshimo69@gmail.com>
2017-04-19 15:11:51 +0900

    cのインデントを4に

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -503,6 +503,7 @@ you should place your code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(c-basic-offset 4)
  '(evil-want-Y-yank-to-eol t)
  '(package-selected-packages
    (quote
