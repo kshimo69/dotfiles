8d9b65bc4a270f549b1040300b534f24065ba9f0
Kimihiko Shimomura <kshimo69@gmail.com>
2017-09-11 10:39:35 +0900

    update osdk script

--- bin/osdk_update.sh
+++ bin/osdk_update.sh
@@ -4,5 +4,4 @@ pushd $OSDK_DIR/RendezVous/Core/setup/RendezVousServer; python setup.py install;
 pushd $OSDK_DIR/RendezVous/Core/setup/RendezVousSwig/; python setup.py install; popd
 pushd $OSDK_DIR/RendezVous/Services/MatchMaking; python setup.py install; popd
 pushd $OSDK_DIR/RendezVous/Services/Jugem/Matchmake; python setup.py install; popd
-pushd $OSDK_DIR/RendezVous/Services/Jugem/Plugin/Coconut; python setup.py install; popd
-pushd $OSDK_DIR/RendezVous/Services/Jugem/Plugin/Calico; python setup.py install; popd
+pushd $OSDK_DIR/RendezVous/Services/Jugem/DataStore/; python setup.py install; popd
