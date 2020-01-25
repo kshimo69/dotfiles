c0b4f1553e3700a38659032f13d4e4e85347354d
Kimihiko Shimomura <kshimo69@gmail.com>
2020-01-25 23:50:43 +0900

    emacsの設定更新

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (magit diminish auto-package-update uniquify org dracula-theme use-package))))
+    (amx which-key exec-path-from-shell gist ripgrep company session magit diminish auto-package-update uniquify org dracula-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -47,36 +47,73 @@
 ;; 常時デバッグ状態
 (setq debug-on-error t)
 
+;; local設定があったら読む
+(when (locate-library "passwd")
+  (require 'passwd))
+
 ;; custom.elを分離
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
 
-;; 文字コード
-(set-language-environment  'utf-8)
-(prefer-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8-unix)
+;; packageの設定
+(require 'package)
+(setq package-enable-at-startup nil)
+;;(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
+
+;; use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package)
+  )
+(progn ; `use-package'
+  ;;(setq use-package-always-defer t)
+  (setq use-package-enable-imenu-support t)
+  (setq use-package-minimum-reported-time 0)
+  (setq use-package-verbose t)
+  (setq use-package-compute-statistics t)
+  (setq use-package-always-ensure t)
+  (require 'use-package)
+  )
+(use-package diminish)
+(use-package bind-key
+  :ensure nil)
+(use-package auto-package-update
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
+
+;;;
+;;; general
+;;;
+
+;; Always load newest byte code
+(setq load-prefer-newer t)
 
 ;; user設定
 (setq user-full-name "Kimihiko Shimomura")
 (setq user-mail-address "kshimo69@gmail.com")
+
+;; 文字コード
+(set-language-environment  'utf-8)
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+
+;; font
+(set-face-attribute 'default nil
+                    :family "Cica"
+                    :height 140)
 
 ;; スタートアップ時のメッセージを抑制
 (setq inhibit-startup-message t)
 
 ;; scratch のメッセージを空にする
 (setq initial-scratch-message nil)
-
-;; Mac用設定
-(when (memq window-system '(mac ns))
-  (setq grep-find-use-xargs 'bsd)
-  (setq browse-url-generic-program "open")
-  (setq ns-command-modifier (quote meta))
-  (setq ns-alternate-modifier (quote super))
-  ;; Ctrl/Cmd/Optionがシステムに渡されるのを防ぐ
-  (setq mac-pass-control-to-system nil)
-  (setq mac-pass-command-to-system nil)
-  (setq mac-pass-option-to-system nil)
-  )
 
 ;; メニューバー、ツールバー、スクロールバー非表示
 (menu-bar-mode +1)
@@ -160,69 +197,11 @@
 ;; カーソル位置のファイル名、URLで開く
 (ffap-bindings)
 
-;;;
-;;; key-bind
-;;;
+;; 終了時に聞く
+(setq confirm-kill-emacs 'y-or-n-p)
 
-;; C-hをバックスペースに
-(global-set-key (kbd "C-h") 'delete-backward-char)
-;; mini-bufferとかどこでも効くように
-;; (keyboard-translate ?\C-h ?\C-?)
-
-;; C-h に割り当てられている関数 help-command を C-x C-h に割り当てる
-(global-set-key (kbd "C-x C-h") 'help-command)
-(global-set-key (kbd "<f1>") 'help-for-help)
-
-;; Scroll buffer without moving the cursor
-(global-set-key (kbd "M-p") '(lambda () (interactive) (scroll-down 1)))
-(global-set-key (kbd "M-n") '(lambda () (interactive) (scroll-up 1)))
-
-;; 略語展開、補完を行うコマンドをまとめる
-(setq hippie-expand-try-functions-list
-      '(try-complete-file-name-partially   ;ファイル名の一部
-        try-complete-file-name             ;ファイル名全体
-        ;; try-expand-all-addrevs             ;静的略語展開
-        try-expand-dabbrev                 ;動的略語展開(カレントバッファ)
-        try-expand-dabbrev-all-buffers     ;動的略語展開(全バッファ)
-        try-expand-dabbrev-from-kill       ;動的略語展開(キルリング)
-        try-complete-lisp-symbol-partially ;Lispシンボルの一部
-        try-complete-lisp-symbol           ;Lispシンボル全体
-        ))
-(global-set-key (kbd "M-/") 'hippie-expand)
-
-;; スクリプトを実行する
-(global-set-key (kbd "C-c p") 'executable-interpret)
-
-;; packageの設定
-(require 'package)
-(setq package-enable-at-startup nil)
-;;(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(package-initialize)
-
-;; use-package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package)
-  )
-(progn ; `use-package'
-  ;;(setq use-package-always-defer t)
-  (setq use-package-enable-imenu-support t)
-  (setq use-package-minimum-reported-time 0)
-  (setq use-package-verbose t)
-  (setq use-package-compute-statistics t)
-  (setq use-package-always-ensure t)
-  (require 'use-package)
-  )
-(use-package diminish)
-(use-package bind-key
-  :ensure nil)
-(use-package auto-package-update
-  :config
-  (setq auto-package-update-delete-old-versions t)
-  (setq auto-package-update-hide-results t)
-  (auto-package-update-maybe))
+(put 'set-goal-column 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
 
 ;; テーマ
 (use-package dracula-theme
@@ -255,12 +234,195 @@
 
 ;; magit
 (use-package magit)
+(use-package gist
+  :config
+  (setq gist-view-gist t)
+  )
 
 (use-package paren
   :config
   (show-paren-mode +1))
 
-;; font
-(set-face-attribute 'default nil
-                    :family "Cica"
-                    :height 140)
+(use-package company
+  :config
+  (global-company-mode)
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 2)
+  (setq company-selection-wrap-around t)
+  (setq x-select-enable-clipboard t)
+  (setq save-interprogram-paste-before-kill t)
+  (setq yank-pop-change-selection t)
+  (setq x-select-enable-clipboard-manager t)
+  (setq x-select-enable-primary t)
+  (setq mouse-drag-copy-region t)
+  )
+
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns))
+  :config
+  ;; (add-to-list 'exec-path-from-shell-variables "NODE_PATH")
+  (exec-path-from-shell-initialize)
+  )
+
+(use-package ripgrep
+  :bind
+  (
+   ("C-c g" . ripgrep-regexp)
+   )
+  :config
+  (setq ripgrep-arguments '("-S"))
+  )
+
+(use-package which-key
+  :diminish which-key-mode
+  :hook (after-init . which-key-mode)
+  )
+
+(use-package amx)
+
+;; kill-ringに同じ内容の文字列を入れない
+(defadvice kill-new (before ys:no-kill-new-duplicates activate)
+  (setq kill-ring (delete (ad-get-arg 0) kill-ring)))
+
+;; Emacsを終了してもファイルを編集してた位置やminibuffer への入力内容を覚えておく
+(use-package session
+  :ensure nil
+  ;;:init
+  ;;(add-hook 'after-init-hook 'session-initialize)
+  :hook (after-init . session-initialize)
+  :config
+  (setq session-save-file (expand-file-name "~/.emacs.d/var/session"))
+  (setq session-initialize '(de-saveplace session keys menus places)
+        session-globals-include '((kill-ring 50)
+                                  (session-file-alist 500 t)
+                                  (file-name-history 10000)))
+  ;; これがないと file-name-history に500個保存する前に max-string に達する
+  (setq session-globals-max-string 100000000)
+  ;; デフォルトでは30!
+  (setq history-length t)
+  )
+
+;; 履歴を次回Emacs起動時にも保存する
+(savehist-mode 1)
+;; (setq savehist-file (expand-file-name "~/.emacs.d/var/history"))
+(setq savehist-file "~/.emacs.d/var/history")
+;; savehistのファイルに保存する履歴からfile-name-historyをのぞく
+(setq savehist-ignored-variables '(file-name-history))
+
+;; ファイル内のカーソル位置を記録する
+(use-package saveplace
+  :config
+  (setq-default save-place t)
+  (setq save-place-file "~/.emacs.d/var/emacs-places")
+  )
+
+;; ログの記録行数を減らす
+(setq message-log-max 10000)
+
+;; *scratch* バッファを消さないように
+(defun my-make-scratch (&optional arg)
+  (interactive)
+  (progn
+    ;; "*scratch*" を作成して buffer-list に放り込む
+    (set-buffer (get-buffer-create "*scratch*"))
+    (funcall initial-major-mode)
+    (erase-buffer)
+    (when (and initial-scratch-message (not inhibit-startup-message))
+      (insert initial-scratch-message))
+    (or arg (progn (setq arg 0)
+                   (switch-to-buffer "*scratch*")))
+    (cond ((= arg 0) (message "*scratch* is cleared up."))
+          ((= arg 1) (message "another *scratch* is created")))))
+(add-hook 'kill-buffer-query-functions
+          ;; *scratch* バッファで kill-buffer したら内容を消去するだけにする
+          (lambda ()
+            (if (string= "*scratch*" (buffer-name))
+                (progn (my-make-scratch 0) nil)
+              t)))
+(add-hook 'after-save-hook
+          ;; *scratch* バッファの内容を保存したら *scratch* バッファを新しく作る
+          (lambda ()
+            (unless (member (get-buffer "*scratch*") (buffer-list))
+              (my-make-scratch 1))))
+
+;;;
+;;; key-bind
+;;;
+
+;; Mac用設定
+(when (memq window-system '(mac ns))
+  (setq grep-find-use-xargs 'bsd)
+  (setq browse-url-generic-program "open")
+  (setq ns-command-modifier (quote meta))
+  (setq ns-alternate-modifier (quote super))
+  ;; Ctrl/Cmd/Optionがシステムに渡されるのを防ぐ
+  (setq mac-pass-control-to-system nil)
+  (setq mac-pass-command-to-system nil)
+  (setq mac-pass-option-to-system nil)
+  )
+
+;; C-hをバックスペースに
+(global-set-key (kbd "C-h") 'delete-backward-char)
+;; mini-bufferとかどこでも効くように
+;; (keyboard-translate ?\C-h ?\C-?)
+
+;; C-h に割り当てられている関数 help-command を C-x C-h に割り当てる
+(global-set-key (kbd "C-x C-h") 'help-command)
+(global-set-key (kbd "<f1>") 'help-for-help)
+
+;; Scroll buffer without moving the cursor
+(global-set-key (kbd "M-p") '(lambda () (interactive) (scroll-down 1)))
+(global-set-key (kbd "M-n") '(lambda () (interactive) (scroll-up 1)))
+
+;; C-t をプリフィックスキー化
+(global-set-key (kbd "C-t") (make-sparse-keymap))
+
+(global-set-key (kbd "C-t C-r") 'window-resizer)
+(global-set-key (kbd "C-t l") 'windmove-right)
+(global-set-key (kbd "C-t h") 'windmove-left)
+(global-set-key (kbd "C-t j") 'windmove-down)
+(global-set-key (kbd "C-t k") 'windmove-up)
+
+;; C-t C-tでウインドウ分割 or 移動
+(defun other-window-or-split ()
+  (interactive)
+  (when (one-window-p)
+    (split-window-horizontally))
+  (other-window 1))
+(global-set-key (kbd "C-t C-t") 'other-window-or-split)
+
+;; カーソル位置の単語をコピー
+(global-set-key (kbd "M-c") 'ffap-copy-string-as-kill)
+
+;; 範囲指定してない時にC-wで前の単語を削除
+(defadvice kill-region (around kill-word-or-kill-region activate)
+  (if (and (interactive-p) transient-mark-mode (not mark-active))
+      (backward-kill-word 1)
+    ad-do-it))
+;; minibuffer用
+(define-key minibuffer-local-completion-map (kbd "C-w") 'backward-kill-word)
+
+;; fullscreen
+(defun toggle-fullscreen ()
+  (interactive)
+  (if (frame-parameter nil 'fullscreen)
+      (set-frame-parameter nil 'fullscreen nil)
+    (set-frame-parameter nil 'fullscreen 'fullboth)
+    ))
+(global-set-key (kbd "M-<RET>") 'toggle-fullscreen)
+
+;; 略語展開、補完を行うコマンドをまとめる
+(setq hippie-expand-try-functions-list
+      '(try-complete-file-name-partially   ;ファイル名の一部
+        try-complete-file-name             ;ファイル名全体
+        ;; try-expand-all-addrevs             ;静的略語展開
+        try-expand-dabbrev                 ;動的略語展開(カレントバッファ)
+        try-expand-dabbrev-all-buffers     ;動的略語展開(全バッファ)
+        try-expand-dabbrev-from-kill       ;動的略語展開(キルリング)
+        try-complete-lisp-symbol-partially ;Lispシンボルの一部
+        try-complete-lisp-symbol           ;Lispシンボル全体
+        ))
+(global-set-key (kbd "M-/") 'hippie-expand)
+
+;; スクリプトを実行する
+(global-set-key (kbd "C-c p") 'executable-interpret)
