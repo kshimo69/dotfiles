dcdbe21881a0ae6d4748130374ece96fb4799e0b
Kimihiko Shimomura <kshimo69@gmail.com>
2020-01-27 18:40:03 +0900

    helmを入れたけどやめるかも

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -284,6 +284,26 @@
   :config
   (show-paren-mode +1))
 
+(use-package helm
+  ;; :init
+  ;; (setq helm-ff-file-name-history-use-recentf t)
+  ;; (setq helm-display-function #'display-buffer)
+  :bind
+  (("M-x" . 'helm-M-x)
+   ("C-;" . helm-mini)
+   ("C-x C-f" . helm-find-files)
+   ("C-c o"   . helm-occur)
+   :map helm-map
+   ("C-h" . delete-backward-char)
+   :map helm-find-files-map
+   ("C-h" . delete-backward-char)
+   ("TAB" . helm-execute-persistent-action)
+   :map helm-read-file-map
+   ("TAB" . helm-execute-persistent-action))
+  :config
+  (helm-mode 1)
+  )
+
 (use-package company
   :config
   (global-company-mode)
