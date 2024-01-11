844916d4208642971936f65924e7ee97bada9acb
Shimomura Kimihiko <shimomura_kimihiko@nintendo.co.jp>
2024-01-11 16:41:13 +0900

    Commentの遅延ロード

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -1,10 +1,11 @@
 return {
   'numToStr/Comment.nvim',
-  event = "VeryLazy",
+  keys = {
+    { ",,", mode = "n", "<Plug>(comment_toggle_linewise_current)", desc = "Comment toggle current line" },
+    { ",,", mode = "x", "<Plug>(comment_toggle_linewise_visual)", desc = "Comment toggle linewise (visual)" },
+  },
   config = function()
     require('Comment').setup()
-    vim.keymap.set('n', ',,', '<Plug>(comment_toggle_linewise_current)', { desc = 'Comment toggle current line' })
-    vim.keymap.set('x', ',,', '<Plug>(comment_toggle_linewise_visual)', { desc = 'Comment toggle current line' })
   end
 }
 
