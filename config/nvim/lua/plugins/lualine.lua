ade1acdd8f2f21bf4b2df656d1e320d17c60bac3
Shimomura Kimihiko <shimomura_kimihiko@nintendo.co.jp>
2024-01-12 16:45:14 +0900

    NvimTreeはlualineを表示しない

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,25 +1,17 @@
 return {
   'nvim-lualine/lualine.nvim',
-  --'vim-airline/vim-airline',
-  --event = { "BufReadPre", "BufNewFile" },
   event = "VimEnter",
   dependencies = {
     'nvim-tree/nvim-web-devicons',
-    --'vim-airline-themes',
-    --'vim-airline/vim-airline-themes',
   },
   config = function()
     require('lualine').setup {
       options = {
         theme = 'iceberg',
+        disabled_filetypes = {
+          statusline = { 'NvimTree'},
+        }
       },
     }
-    --[[
-    vim.cmd[[
-      let g:airline_theme = 'iceberg'
-      " タブにも適用
-      let g:airline#extensions#tabline#enabled = 1
-      set laststatus=2
-    ]]
   end
 }
