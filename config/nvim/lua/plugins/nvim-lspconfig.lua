1de3f7d3f6339c6c309ee3810acddc8b2bfb08d6
Shimomura Kimihiko <shimomura_kimihiko@nintendo.co.jp>
2024-01-14 22:43:27 +0900

    lsp-configの設定

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -8,7 +8,23 @@ return {
       'williamboman/mason.nvim',
       config = true,
     },
-    'williamboman/mason-lspconfig.nvim',
+    {
+      'williamboman/mason-lspconfig.nvim',
+      config = function()
+        local mason_lspconfig = require("mason-lspconfig")
+        mason_lspconfig.setup({
+          ensure_installed = { "lua_ls" },
+        })
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+        mason_lspconfig.setup_handlers({function(server_name)
+            require('lspconfig')[server_name].setup {
+              capabilities = capabilities,
+            }
+          end,
+        })
+      end,
+    },
     {
       'lukas-reineke/lsp-format.nvim',
       config = function()
