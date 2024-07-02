0b6b116807f599d634925d1db538361976965163
Shimomura Kimihiko <shimomura_kimihiko@nintendo.co.jp>
2024-07-02 21:01:24 +0900

    nvim-treeのヘルプが見れるように

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -6,7 +6,17 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
+    local function my_on_attach(bufnr)
+      local api = require('nvim-tree.api')
+      local function opts(desc)
+        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+      api.config.mappings.default_on_attach(bufnr)
+      vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+    end
+    require("nvim-tree").setup {
+      on_attach = my_on_attach,
+    }
     vim.keymap.set('n', '<leader>ee', function() vim.cmd("NvimTreeFindFile") end)
-    require("nvim-tree").setup {}
   end,
 }
