47a507a670d87db059cc03f51e20b20c5c143a81
Shimomura Kimihiko <shimomura_kimihiko@nintendo.co.jp>
2024-07-02 21:08:06 +0900

    jaq-nvimを追加

---
+++ config/nvim/lua/plugins/jaq.lua
@@ -0,0 +1,77 @@
+return {
+  'is0n/jaq-nvim',
+  event = "VeryLazy",
+  config = function()
+    require('jaq-nvim').setup {
+      cmds = {
+        -- Uses vim commands
+        internal = {
+          lua = "luafile %",
+          vim = "source %"
+        },
+
+        -- Uses shell commands
+        external = {
+          markdown = "glow %",
+          python   = "python3 %",
+          go       = "go run %",
+          sh       = "bash %"
+        }
+      },
+
+      behavior = {
+        -- Default type
+        default     = "float",
+
+        -- Start in insert mode
+        startinsert = false,
+
+        -- Use `wincmd p` on startup
+        wincmd      = false,
+
+        -- Auto-save files
+        autosave    = false
+      },
+
+      ui = {
+        float = {
+          -- See ':h nvim_open_win'
+          border   = "none",
+
+          -- See ':h winhl'
+          winhl    = "Normal",
+          borderhl = "FloatBorder",
+
+          -- See ':h winblend'
+          winblend = 0,
+
+          -- Num from `0-1` for measurements
+          height   = 0.8,
+          width    = 0.8,
+          x        = 0.5,
+          y        = 0.5
+        },
+
+        terminal = {
+          -- Window position
+          position = "bot",
+
+          -- Window size
+          size     = 10,
+
+          -- Disable line numbers
+          line_no  = false
+        },
+
+        quickfix = {
+          -- Window position
+          position = "bot",
+
+          -- Window size
+          size     = 10
+        }
+      }
+    }
+    vim.keymap.set("n", "<leader>ll", ":<C-u>Jaq<CR>", { silent = true })
+  end
+}
